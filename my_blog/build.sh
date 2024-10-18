6d3f7e85470e0730a6cf71ac46828f130d6a3ef8
Neeraj <neerajsinghchauhan0000@gmail.com>
2024-10-18 23:07:01 +0530

    third

--- my_blog/build.sh
+++ my_blog/build.sh
@@ -10,8 +10,6 @@ echo "Installing dependencies from requirements.txt"
 
 pip install -r requirements.txt
 
-# Navigate to the project directory
-cd my_blog
 
 # Collect static files
 python manage.py collectstatic --no-input
